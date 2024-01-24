ff46bbcc34766ca55a1c906814cc85980dc96bdf
diamond_khushi <66999473+khushikesarwani@users.noreply.github.com>
2024-01-24 15:00:35 +0530

    Update server.js back to original

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ import {fileURLToPath} from 'url';
 dotenv.config()
 
 //database config
-// connectDB();
+connectDB();
 
 const app=express();
 //es module fix
@@ -42,8 +42,8 @@ res.sendFile(path.join(__dirname,'./client/build/index.html'));
 const PORT=process.env.PORT ||8080;
 
 
-connectDB().then(() => {
+
     app.listen(PORT, () => {
         console.log("server is listening for requests");
-    })
-});
+    });
+
